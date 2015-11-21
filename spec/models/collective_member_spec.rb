eab04b2177a95650c6ea227c68e83de15be0db76
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-21 09:00:17 -0500

    basic stuff yo

--- spec/models/collective_member_spec.rb
+++ spec/models/collective_member_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CollectiveMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  do "it should have a valid factory" do
+    member = FactoryGirl.create(:collective_member)
+  end
 end
