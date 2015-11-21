b3777e4a2d504877ef7e0c9fad694c3b398204a4
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-21 08:51:55 -0500

    the specs (omg the generator spits out so many)

---
+++ spec/controllers/collective_members_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe CollectiveMembersController, type: :controller do
+
+  let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+  }
+
+  let(:invalid_attributes) {
+    skip("Add a hash of attributes invalid for your model")
+  }
+
+  let(:valid_session) { {} }
+
+  end
---
+++ spec/models/collective_member_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe CollectiveMember, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
