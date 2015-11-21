9a7eac1b6648cb8ffeace6e2ea3ca469d9d6be3b
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-21 08:59:42 -0500

    factory for collective members

---
+++ spec/factories/collective_member.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :collective_member do
+    first_name "Karl"
+    last_name "Marx"
+    email_address "the_spectre@uni-jena.de"
+    phone_number "2345678901"
+    trainer true
+  end
+end
