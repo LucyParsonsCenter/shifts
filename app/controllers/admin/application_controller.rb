d12b127a28145c5e1306f61c83f2fa2a6fc54835
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-22 18:36:03 -0500

    authenticate before we do anything in the admin controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,19 +1,7 @@
-# All Administrate controllers inherit from this `Admin::ApplicationController`,
-# making it the ideal place to put authentication logic or other
-# before_filters.
-#
-# If you want to add pagination or other controller-level concerns,
-# you're free to overwrite the RESTful controller actions.
 class Admin::ApplicationController < Administrate::ApplicationController
-  before_filter :authenticate_admin
+  before_action :authenticate_user!
 
-  def authenticate_admin
-    # TODO Add authentication logic here.
+  def records_per_page
+    params[:per_page] || 30
   end
-
-  # Override this value to specify the number of elements to display at a time
-  # on index pages. Defaults to 20.
-  # def records_per_page
-  #   params[:per_page] || 20
-  # end
 end
