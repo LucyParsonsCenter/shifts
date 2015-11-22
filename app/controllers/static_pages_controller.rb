2558f551d4e204adbf59afe393239af37e72a8dd
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-22 14:32:14 -0500

    quick help static page (fill in later)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,8 @@ class StaticPagesController < ApplicationController
   def home
     render :home
   end
+
+  def help
+    render :help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   get '/' => 'static_pages#home'
   get '/home' => 'static_pages#home'
+  get '/help' => 'static_pages#help'
 end
