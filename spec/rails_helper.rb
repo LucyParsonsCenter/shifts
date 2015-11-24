806aebf4ce03efff0e678c1defb59ca95bf499a8
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-23 19:27:40 -0500

    adding the spec helper for devise (gives us sign_in_as, etc)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.use_transactional_fixtures = true
+  config.include Devise::TestHelpers, type: :controller
 
   config.infer_spec_type_from_file_location!
 
