5aaaec43ad346421105ce4763308b13701e1a21e
Alice Pote <alice.writes.wrongs@gmail.com>
2015-11-28 18:30:06 -0500

    scopes on shifts

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,7 @@
 class Shift < ActiveRecord::Base
   belongs_to :collective_member
   belongs_to :trainee
+
+  scope :training, -> { where.not(trainee: nil) }
+  scope :normal, -> { where(trainee: nil) }
 end
