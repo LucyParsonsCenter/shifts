c96ae20f045da58754b9ae9acead3f039dca8e73
Alice Pote <alice.writes.wrongs@gmail.com>
2015-12-12 11:32:50 -0500

    fixed event scopes to reflect db change

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ActiveRecord::Base
-  has_many :collective_member
-  has_many :trainee
+  has_many :collective_members
+  has_many :trainees
 
   scope :meeting, -> { where(meeting: true) }
-  scope :training, -> { where.not(trainee: nil) }
-  scope :normal, -> { where(trainee: nil).where(meeting: false) }
+  scope :training, -> { where.not(trainee_id: nil) }
+  scope :normal, -> { where(trainee_id: nil).where(meeting: false) }
 
   def format
     event = Hash.new
