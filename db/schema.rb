38b7fa44676ff0b79756547e3bbae23e1b13c9ae
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-03 21:37:55 -0500

    add a title attribute to the event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212164535) do
+ActiveRecord::Schema.define(version: 20160104023608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151212164535) do
     t.datetime "end_time",                   null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.string   "title"
   end
 
   create_table "events_trainees", id: false, force: :cascade do |t|
