0e102681d0bd31c28760b1ad714f24b7bc675e53
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-16 13:14:44 -0500

    a method! it converts a trainee to a collective member

--- app/models/trainee.rb
+++ app/models/trainee.rb
@@ -5,4 +5,12 @@ class Trainee < ActiveRecord::Base
     {value: self.id, label: "#{self.first_name} #{self.last_name}"}
   end
 
+  def convert_to_collective_member
+    trainee = self.serializable_hash
+    trainee.delete("notes")
+    trainee.delete("id")
+    CollectiveMember.create!(trainee)
+    Trainee.find_by_id(self.id).delete!
+  end
+
 end
