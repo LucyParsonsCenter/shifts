dd6d17b63eefb4107eb79faf8dc113c9971d03ff
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-01 14:06:07 -0500

    add route and method for letsencrypt verification

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,8 @@ class StaticPagesController < ApplicationController
   def about
     render :about
   end
+
+  def letsencrypt
+    render text: "KCQ0kRgreeQjSl7C4358AX7eVrYl_GUicPVR3hUThMw.vyei9LUnoD7AxLw6GOcsWiaTNQBADMMpSYOu8Yb57V8"
+  end
 end
