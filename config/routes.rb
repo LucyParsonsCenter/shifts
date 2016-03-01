26296d40b0004c0b67007047230896c03393c305
Alice Pote <alice.writes.wrongs@gmail.com>
2016-02-29 21:05:56 -0500

    tear out all the administrate stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations" }
   root to: 'static_pages#home'
 
-  namespace :admin do
-    DashboardManifest::DASHBOARDS.each do |dashboard_resource|
-      resources dashboard_resource
-    end
-
-    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
-  end
-
   resources :events
   resources :collective_members
 
